763f4536e400a342cc3096e41895e508d7299e11
Mateusz Borycki <mateuszborycki@gmail.com>
2022-04-22 15:54:06 +0200

    Updating tests.

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    // const ms: string = core.getInput('milliseconds')
+    const credentials = core.getInput('credentials')
+    const filename = core.getInput('filename')
+    const folderId = core.getInput('folderId')
+
+    core.notice(`Uploading ${filename} to ${folderId}...`)
+
+    const ms = "1000";
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
     core.debug(new Date().toTimeString())
